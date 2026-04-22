7839f3d1e649fefc99f1eff5272b0c7ddc7ee148
samasama11111 <samasama111115@gmail.com>
2026-04-22 15:55:50 +0300

    refactor(update_delivery): change location data structure from JSON encoding to a map for improved clarity and performance

--- lib/features/delivery/update_delivery/data/data_sources/update_delivery_data_source.dart
+++ lib/features/delivery/update_delivery/data/data_sources/update_delivery_data_source.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:jeeb_admin/core/infrastructure/api/api_service.dart';
 
@@ -59,7 +57,7 @@ class UpdateDeliveryRemoteDataSourceImpl
       if (notificationChannel != null && notificationChannel.isNotEmpty)
         'notificationChannel': notificationChannel,
       if (latitude != null && longitude != null)
-        'location': jsonEncode({'lat': latitude, 'lng': longitude}),
+        'location': {'lat': latitude, 'lng': longitude},
       if (imagePath != null && imagePath.isNotEmpty)
         'image': await MultipartFile.fromFile(imagePath),
     });
